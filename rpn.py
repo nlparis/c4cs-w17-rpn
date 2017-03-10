4addd27b5dbcce804ebbf0c23fac9a0357c2089c
nlparis <nick.paris987@gmail.com>
2017-03-10 15:42:56 -0500

    working plus, minus, and divide for rpn.py

--- rpn.py
+++ rpn.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 
+import operator
+
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'/': operator.truediv,
+}
+
 def calculate(arg):
-	pass
+	stack = list()
+	for operand in arg.split():
+		try:
+			operand = float(operand)
+			stack.append(operand)
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			operator_fn = operators[operand]
+			result = operator_fn(arg1, arg2)
+			stack.append(result)
+		
+	return stack.pop()
 
 def main():
 	while True:
-		calculate(input('rpn calc> '))
+		result = calculate(input('rpn calc> '))
+		print('result:', result)
 
 if __name__ == '__main__':
 	main()
--- test_rpn.py
+++ test_rpn.py
@@ -3,6 +3,11 @@ import rpn
 
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-		result = rpn.calculate('1 1 +')
+		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
-
+	def test_subtract(self):
+		result = rpn.calculate("5 3 -")
+		self.assertEqual(2, result)
+	def test_divide(self):
+		result = rpn.calculate("4 4 /")
+		self.assertEqual(1, result)
